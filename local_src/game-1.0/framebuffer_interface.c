2351264b7ccbd02c499cb69ede44311bf906e790
Kevin <kevinvin@stud.ntnu.no>
2019-11-05 20:18:37 +0100

    Added module for framebuffer interface

---
+++ local_src/game-1.0/framebuffer_interface.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "framebuffer_interface.h"
+
+void InitInterface(void)
+{
+
+}
+
+void DrawPixel(int x, int y)
+{
+
+}
+
+void ClearScreen(void)
+{
+
+}
+
+void UpdateScreen(void)
+{
+
+}
