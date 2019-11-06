4477d991b573d353eb8422da6d7a6cabcdf1d58d
Kevin <kevinvin@stud.ntnu.no>
2019-11-06 15:38:43 +0100

    Working (but slow) code for writing to framebuffer

--- local_src/game-1.0/framebuffer_interface.h
+++ local_src/game-1.0/framebuffer_interface.h
@@ -4,34 +4,12 @@
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 240
 #define BYTES_PER_PIXEL 2
-#define COLOR_WHITE 0xFF //Check this value!
 
-
-/*  Initialization of module
- *  Input:  none
- *  Output: none
- */
-void InitInterface(void);
-
-/*  Draws a singular (white) pixel to the screen.
- *  (Note: call 'UpdateScreen' for changes to show.)
+/*  Draws a singular pixel to the screen.
  *  Input:  int x: x-coordinate
  			int y: y-coordinate
  *  Output: none
  */
 void DrawPixel(int x, int y);
-
-/*  Clear screen to default color (black).
- *  (Note: call 'UpdateScreen' for changes to show.)
- *  Input:  none
- *  Output: none
- */
-void ClearScreen(void);
-
-/*  Termination of module (clean exit).
- *  Input:  none
- *  Output: none
- */
-void TerminateInterface(void);
 
 #endif
--- local_src/game-1.0/game.c
+++ local_src/game-1.0/game.c
@@ -1,65 +1,22 @@
-// CURRENTLY FUNCTIONS AS A TESTBENCH FOR GWINT.C
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> //For sleep function; debug
 
-#include "drawapi.h"
-
-//Game state
-int ballx = WINDOW_W/2;
-int bally = WINDOW_H/2;
-
-//Draw game components
-
-void drawPaddle(int x, int y) {
-	drawRectangle(x, y, 15, 100);
-}
-
-void drawLeftPaddle() {
-	drawPaddle(10, WINDOW_H/2-50);
-}
-
-void drawRightPaddle() {
-	drawPaddle(WINDOW_W-10-30, WINDOW_H/2-50);
-}
-
-void drawBall() {
-	drawRectangle(ballx, bally, 20, 20);
-}
-
-//Function for debugging the gwint module
-void dbg_gwint(int argc, char** argv)
-{
-	printf("gwinit\n");
-	INIT_DRAWING(&argc, argv);
-	int count = 0;
-	printf("Drawing loop\n");
-	while(1)
-	{
-		ClearWindow();
-
-		drawLeftPaddle();
-		drawRightPaddle();
-		drawBall();
-
-		UpdateWindow();
-		usleep(1000*100);
-		if(count > 100) {
-			count = 0;
-		}
-		count++;
-	}
-}
+#include "framebuffer_interface.h"
 
 int main(int argc, char** argv) {
-	setbuf(stdout, NULL);
-	printf("Hello, world");
-	dbg_gwint(argc, argv);
-	while(1)
+	printf("Start game!\n");
+	int x;
+	int y;
+	for (x = 100; x < 150; x++)
 	{
-		//nothing
+		for (y = 100; y < 150; y++)
+		{
+			DrawPixel(x, y);
+		}
 	}
+	printf("All pixels drawn\n");
+	printf("Goodbye!\n");
     return 0;
 }
 
