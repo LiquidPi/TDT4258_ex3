16c503dcb3707c50fee44a9b80320c9c79d19ef3
Liam S. Crouch <spam@petterroea.com>
2019-11-20 15:28:56 +0100

    math

--- local_src/game-1.0/game.c
+++ local_src/game-1.0/game.c
@@ -95,26 +95,8 @@ void someoneWon(int winner) {
 	ballDirection[1] = (float)(rand()%100)/100.0f-0.5f;
 }
 
-//Fast sqrt from doom, so we don't have to include any more libraries(Make was mean)
-float Q_rsqrt( float number )
-{
-	long i;
-	float x2, y;
-	const float threehalfs = 1.5F;
-
-	x2 = number * 0.5F;
-	y  = number;
-	i  = * ( long * ) &y;                       // evil floating point bit level hacking
-	i  = 0x5f3759df - ( i >> 1 );               // what the fuck? 
-	y  = * ( float * ) &i;
-	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
-//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
-
-	return y;
-}
-
 void normalizeBallDirection() {
-	float normalizedScalar = Q_rsqrt(ballDirection[0]*ballDirection[0]+ballDirection[1]*ballDirection[1]);
+	float normalizedScalar = sqrt(ballDirection[0]*ballDirection[0]+ballDirection[1]*ballDirection[1]);
 	ballDirection[0] /= normalizedScalar;
 	ballDirection[1] /= normalizedScalar;
 }
